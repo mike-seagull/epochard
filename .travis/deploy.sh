f0f02801a58479335d88c07815652492049b70f5
Michael Hollister <hollister.michael@gmail.com>
2019-02-18 23:37:04 -0600

    moved deployment files to dropbox

--- .travis/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-scp  -o LogLevel=quiet $(pwd)/bin/epochard ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/epochard
-ssh  -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} /bin/chmod +x /usr/local/bin/epochard
